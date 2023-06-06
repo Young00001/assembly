97b5d492ddda87ef4f0cf67839cd7b5c8b0439b4
null <null@framework.mynetworksettings.com>
2023-06-06 01:10:55 -0400

    Assembly Code

--- hello_world.asm
+++ hello_world.asm
@@ -4,8 +4,23 @@
 ; Date: 05-June-2023
 ;
 
+global _start
+
 section .text:
 
+_start:
+	mov eax, 0x4			; use the write syscall
+	mov ebx, 1			; use stdout as the file descriptor
+	mov ecx, message		; use the message as the buffer
+	mov edx, message_length		; supply the length
+	int 0x80			; invoke the syscall
+
+	; exit
+
+	mov eax, 0x1
+	mov ebx, 0
+	int 0x80
 
 section .data:
-	message: "Hello World!", 0xA
+	message: db "Hello World!", 0xA
+	message_length equ $-message
